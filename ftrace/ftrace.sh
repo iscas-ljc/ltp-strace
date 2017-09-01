ec061b8087e2e3c829181e92b8cf72e798841d9a
iscas-ljc <861218470@qq.com>
2017-09-01 15:33:29 +0800

    ftrace

--- ftrace/ftrace.sh
+++ ftrace/ftrace.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-sudo trace-cmd record -p function ./$1
+sudo trace-cmd record -p function $1
 sudo trace-cmd report trace.dat > 1.txt
+#提取文件名
+a=$1
+a=${a:2}
 #删除文中空格
 sudo sed -i 's/[[:space:]]//g' 1.txt
 #删除无关函数(保留匹配行)
-sudo sed -i -n '/^'$1'/p' 1.txt
+sudo sed -i -n '/^'$a'/p' 1.txt
 #提取函数
 sudo sed -i 's/^.*://' 1.txt
 #整理函数
-sudo sort 1.txt | uniq > ../../../../../home/iscas-ljc/doc/fun_$1.txt 
+sudo sort 1.txt | uniq > /home/iscas-ljc/doc/fun_"$a".txt 
 #统计函数
-sudo sort 1.txt | uniq -c > ../../../../../../home/iscas-ljc/doc/num_fun_$1.txt
+sudo sort 1.txt | uniq -c > /home/iscas-ljc/doc/num_fun_"$a".txt
 #删除中间文件
 sudo rm 1.txt
