90fce0b301fb8fd7c15181a239953067f09168f0
brodan <861218470@qq.com>
2017-09-05 22:33:09 -0700

    ftrace

--- ftrace/ftrace_group.sh
+++ ftrace/ftrace_group.sh
@@ -8,16 +8,19 @@ sudo trace-cmd report trace.dat > 1.txt
 #提取文件名
 a=$file
 a=${a:2}
+#删除前两行
+sudo sed -i '1d' 1.txt
+sudo sed -i '2d' 1.txt
 #删除文中空格
 sudo sed -i 's/[[:space:]]//g' 1.txt
 #删除无关函数(保留匹配行)
-sudo sed -i -n '/^'$a'/p' 1.txt
+#sudo sed -i -n '/^'$a'/p' 1.txt
 #提取函数
 sudo sed -i 's/^.*://' 1.txt
 #整理函数
-sudo sort 1.txt | uniq > /home/iscas-ljc/doc/fun_"$a".txt 
+sudo sort 1.txt | uniq > /home/brodan/doc/fun_"$a".txt 
 #统计函数
-#sudo sort 1.txt | uniq -c > /home/iscas-ljc/doc/num_fun_"$a".txt
+#sudo sort 1.txt | uniq -c > /home/brodan/doc/num_fun_"$a".txt
 #删除中间文件
 sudo rm 1.txt
 sudo rm trace.dat
