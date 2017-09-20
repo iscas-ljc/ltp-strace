da584994b68c523f33199011b4d4ed804658313c
brodan <861218470@qq.com>
2017-09-20 13:40:58 +0800

    shell

--- shell/merge.sh
+++ shell/merge.sh
@@ -4,8 +4,12 @@ for file in $1*
 do 
     if test -f $file 
     then
+        sed -i 's/^[ ]*//g' $file
+        #删除空格
         sudo cat $file >> fin.txt
     fi
 done
-sort fin.txt | uniq -c > result.txt
+sort fin.txt | uniq -c > fin.txt
+sort -k1 -n fin.txt>result.txti
+#k1表示用第一段进行排序 n表示数值大小排序 nr表示数值大小倒序
 rm fin.txt
